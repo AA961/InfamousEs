b26bbe09610d388b7a3d4610627c1c7487d89f29
AA961 <ammar.ahmed961@gmail.com>
2022-05-04 17:35:02 +0500

    js

--- javascript/app.js
+++ javascript/app.js
@@ -10,5 +10,5 @@ toggleMenu.onclick = function(){
 
 navigation.addEventListener('click',function(){
     navigation.classList.remove('active')
-    toggleMenu.classList.toggle('active')
+    toggleMenu.classList.remove('active')
 })
