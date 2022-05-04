b623a3a959dbe10e42919816d2b7f6b092881de0
AA961 <ammar.ahmed961@gmail.com>
2022-05-04 17:20:37 +0500

    nav

---
+++ javascript/app.js
@@ -0,0 +1,13 @@
+let toggleMenu = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+
+
+toggleMenu.onclick = function(){
+    toggleMenu.classList.toggle('active')
+    navigation.classList.toggle('active')
+
+}
+
+navigation.addEventListener('click',function(){
+    navigation.classList.remove('active')
+})
